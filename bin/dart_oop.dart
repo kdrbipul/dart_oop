30578ce77448d758a64b03589d9aae1cf2da6273
kdrbipul <kdrbipul@gmail.com>
2024-07-10 12:49:33 +0600

    method add in class

--- bin/dart_oop.dart
+++ bin/dart_oop.dart
@@ -7,10 +7,25 @@ studentOne.address = "rampura-5401, ranpgur";
 print(studentOne.name);
 print(studentOne.age);
 print(studentOne.address);
+
+Student studentTwo = Student();
+studentTwo.name = 'tonmoy';
+studentTwo.age = 25;
+studentTwo.address = "satgara-5401, rangpur";
+
+print(studentTwo.name);
+print(studentTwo.age);
+print(studentTwo.address);
+studentTwo.coding();
 }
 
 class Student{
   String name = '';
   int age = 0;
   String address = '';
+
+
+  void coding(){
+    print('$name coding');
+  }
 }
