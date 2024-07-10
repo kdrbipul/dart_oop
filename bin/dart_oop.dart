48aaab251248d8122190840a01bfa160f4822e7a
kdrbipul <kdrbipul@gmail.com>
2024-07-10 14:51:00 +0600

    method parameters & method arguments add

--- bin/dart_oop.dart
+++ bin/dart_oop.dart
@@ -1,5 +1,5 @@
 void main(){
-Student studentOne = Student();
+Student studentOne = Student();  //class object
 studentOne.name = 'sakib';
 studentOne.age = 23;
 studentOne.address = "rampura-5401, ranpgur";
@@ -8,7 +8,7 @@ print(studentOne.name);
 print(studentOne.age);
 print(studentOne.address);
 
-Student studentTwo = Student();
+Student studentTwo = Student();  //class object
 studentTwo.name = 'tonmoy';
 studentTwo.age = 25;
 studentTwo.address = "satgara-5401, rangpur";
@@ -17,15 +17,25 @@ print(studentTwo.name);
 print(studentTwo.age);
 print(studentTwo.address);
 studentTwo.coding();
+studentOne.coding();
+studentTwo.doing('solution of calculus'); //method arguments
 }
 
+
+//class
 class Student{
+  //properties/attributes
   String name = '';
   int age = 0;
   String address = '';
 
-
+  //method
   void coding(){
     print('$name coding');
+  }
+
+  //method with parameters
+  void doing(String homeWork) {
+    print('$name is doing $homeWork');
   }
 }
